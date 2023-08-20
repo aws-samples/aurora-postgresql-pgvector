2df3d3ad437a336367b5f59804e2d64cf658c54b
shayons <57544509+shayons@users.noreply.github.com>
2023-08-20 01:51:56 -0400

    Update app.py

--- apgpgvector-streamlit/app.py
+++ apgpgvector-streamlit/app.py
@@ -21,7 +21,7 @@ def get_pdf_text(pdf_docs):
 
 def get_text_chunks(text):
     text_splitter = RecursiveCharacterTextSplitter(
-        separators=["\n\n", "\n", ".", "!", "?", ",", " ", ""],
+        separators=["\n\n", "\n", ".", " "],
         chunk_size=1000,
         chunk_overlap=200,
         length_function=len
