01f365f2fc31128172e82d151729ee5223e4028e
kmsarabu <krishna.sarabu@gmail.com>
2024-03-05 22:16:07 -0800

    Movie Recommendations using Aurora ML and Amazon Bedrock

--- 04_AuroraML_Bedrock_MovieRecommendations/app.py
+++ 04_AuroraML_Bedrock_MovieRecommendations/app.py
@@ -6,84 +6,40 @@ import os
 import json
 from dotenv import load_dotenv
 
-st.write("## :orange[Movie Recommendations :cinema:]")
-
-@st.cache_data
-def load_data():
-    with psycopg2.connect(database=dbname, host=dbhost, port=dbport, user=dbuser, password=dbpass) as dbconn:
-        sql = "SELECT id, title FROM movie.movies;"
-        return pd.read_sql_query(sql, dbconn, index_col="id")
-
 def write_columns_data(result):
     col1, col2, col3, col4, col5 = st.columns(5)
-    col1.image("https://image.tmdb.org/t/p/w185{}".format(result[0].get('poster')))
-    col2.image("https://image.tmdb.org/t/p/w185{}".format(result[1].get('poster')))
-    col3.image("https://image.tmdb.org/t/p/w185{}".format(result[2].get('poster')))
-    col4.image("https://image.tmdb.org/t/p/w185{}".format(result[3].get('poster')))
-    col5.image("https://image.tmdb.org/t/p/w185{}".format(result[4].get('poster')))
+    colarray = [ col1, col2, col3, col4, col5 ]
+    for x in range(5):
+        colarray[x].image("https://image.tmdb.org/t/p/w185{}".format(result[x+1].get('poster')))
     return
 
-def get_review_summary(option, dbconn):
-        qry = """WITH 
-                    p AS ( 
-                        SELECT '\\n\\nHuman: Please provide a summary of the following text.\\n<text>\\n{doc_text}\\n </text>\\n\\nAssistant:' AS prompt
-                        )
-                    , m AS (
-                        SELECT 
-                            id, 
-                            regexp_replace(
-                            regexp_replace(regexp_replace(regexp_replace(STRING_AGG(review, '\\n'), E'[\\n\\r]+', '\\n', 'g'),
-                            '[\\10|/10]', ' out of 10', 'g'), $$['\"-]$$, '', 'g'), '[^[:ascii:]]', '', 'g')  
-                            AS reviews 
-                        FROM movie.reviews
-                        WHERE id = (select id from movie.movies where title = %s limit 1)
-                        GROUP BY id
-                        )
-                    SELECT
-                        aws_bedrock.invoke_model(
-                            model_id        := 'anthropic.claude-v2',
-                            content_type    := 'application/json',
-                            accept_type  := 'application/json',
-                            model_input  := '{\"prompt": \"'|| replace(p.prompt, '{doc_text}', m.reviews) || '\", \"max_tokens_to_sample\": 4096, \"temperature\": 0.5, \"top_k\": 250, \"top_p\": 0.5, \"stop_sequences\":[] }'
-                        )
-                    FROM m cross join p;"""
-
-        with dbconn.cursor(cursor_factory = psycopg2.extras.RealDictCursor) as tcur:
-            tcur.execute(qry, (option, ))
-            result = tcur.fetchall()
-            if result:
-                return json.loads( result[0].get('invoke_model') )
-            return {'completion': ''}
-
 def main():
-    df = load_data()
-    option = st.selectbox('##### :orange[Select a movie you watched?]', df.title.unique())
-    st.write('You have selected :orange[', option, ']')
-    st.divider()
-    with psycopg2.connect(database=dbname, host=dbhost, port=dbport, user=dbuser, password=dbpass) as dbconn:
-        dbcur = dbconn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
-        dbcur.execute("SELECT m.id, m.title, m.poster, m.overview FROM movie.movies m WHERE title = %s", (option, ))
-        result = dbcur.fetchall()
-        col1, col2, col3 = st.columns(3)
-        with col1:
-            st.header(option)
-            st.image("https://image.tmdb.org/t/p/w185{}".format(result[0].get('poster')))
-        with col2:
-            st.header("Story")
-            st.write(result[0].get('overview'))
-        with col3:
-            st.header("Summary of user reviews")
-            res = get_review_summary(option, dbconn)
-            st.write(res.get('completion'))
-        
-    st.divider()
-    with st.container():
-        st.write("##### :green[Personalized Movie Recommendations]")
+    st.title(':orange[Movie Catalog Demo :cinema:]')
+    query = st.text_input('Search for a movie')
+    if query:
         with psycopg2.connect(database=dbname, host=dbhost, port=dbport, user=dbuser, password=dbpass) as dbconn:
+            st.divider()
+            st.subheader('Top Matching Movie:')
             dbcur = dbconn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
-            dbcur.execute("SELECT m.id, m.title, m.poster FROM movie.movies m WHERE title <> %s ORDER BY m.movie_embedding <-> (SELECT movie_embedding FROM movie.movies WHERE title = %s LIMIT 1)  LIMIT 5;", (option, option, ))
+            dbcur.execute("SELECT * FROM movie.get_top6_movies(%s);", (query,))
             result = dbcur.fetchall()
-            write_columns_data(result)
+            col1, col2, col3 = st.columns(3)
+            with col1:
+                st.subheader(result[0].get('title'))
+                st.image("https://image.tmdb.org/t/p/w185{}".format(result[0].get('poster')))
+            with col2:
+                st.subheader("Story")
+                st.write(result[0].get('overview'))
+            with col3:
+                st.subheader("Summary of user reviews")
+                dbcur.execute("SELECT movie.get_reviews_summary(%s)", (result[0].get('id'), ))
+                res = dbcur.fetchall()
+                if res:
+                    st.write( res[0].get('get_reviews_summary').get('completion').replace(' Here is a summary of the key points from the text:\n\n-', '') )
+            st.divider()
+            st.subheader('Top 5 Recommended Movies:')
+            with st.container():
+                write_columns_data(result)
         st.divider()
         
 if __name__ == '__main__':
