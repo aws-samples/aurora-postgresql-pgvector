52a5175da9172155fa9f9472a5e06017fc23c2ae
shayons <57544509+shayons@users.noreply.github.com>
2024-05-03 20:25:40 -0400

    Update app.py

--- 02_RetrievalAugmentedGeneration/02_QuestionAnswering_Bedrock_LLMs/app.py
+++ 02_RetrievalAugmentedGeneration/02_QuestionAnswering_Bedrock_LLMs/app.py
@@ -8,8 +8,7 @@ from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import BedrockEmbeddings
-from langchain_community.llms import Bedrock
-from langchain_community.chat_models import BedrockChat
+from langchain_aws import ChatBedrock
 from langchain_core.prompts import PromptTemplate
 import streamlit as st
 import boto3
@@ -57,7 +56,7 @@ def get_vectorstore(text_chunks):
 # This chain allows the Gen AI app to engage in conversational interactions.
 def get_conversation_chain(vectorstore):
     # Define model_id, client and model keyword arguments for Anthropic Claude v3
-    llm = BedrockChat(model_id="anthropic.claude-3-sonnet-20240229-v1:0", client=BEDROCK_CLIENT)
+    llm = ChatBedrock(model_id="anthropic.claude-3-sonnet-20240229-v1:0", client=BEDROCK_CLIENT)
     llm.model_kwargs = {"temperature": 0.5, "max_tokens": 8191}
     
     # The text that you give Claude is designed to elicit, or "prompt", a relevant output. A prompt is usually in the form of a question or instructions. When prompting Claude through the API, it is very important to use the correct `\n\nHuman:` and `\n\nAssistant:` formatting.
@@ -224,6 +223,6 @@ if __name__ == '__main__':
     BEDROCK_CLIENT = boto3.client("bedrock-runtime", 'us-west-2')
     
     # Create the connection string for pgvector. Ref: https://github.com/langchain-ai/langchain-postgres/blob/main/examples/vectorstore.ipynb
-    connection = "postgresql+psycopg://<user name>:<enter your password>@<enter your Aurora PG cluster>:5432/<enter your DB name>"
+    connection = "postgresql+psycopg://<username>:<password>@<DB host>:5432/<DB name>"
 
     main()
